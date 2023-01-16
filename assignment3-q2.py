58c9f5276a41b7d5095ace0f4833a2457357d947
Elaheh Aria <122198974+ElahahAria@users.noreply.github.com>
2023-01-16 21:36:01 +0330

    Add files via upload

---
+++ assignment3-q2.py
@@ -0,0 +1,20 @@
+import random
+
+while True:
+
+   Roll = input("Enter 'R' to roll the dice :")
+   if Roll == 'R':
+        Dice = random.randint(1,6)
+        if 1 <= Dice <= 5:
+            print("The dice shows =" , Dice)
+        
+        if Dice == 6:
+            Bonus_dice_1 = random.randint(1,6)
+            Bonus_dice_2 = random.randint(1,6)
+            print("Congratulation! You got a lucky 6! , First bonus dice =",Bonus_dice_1 , ", Second bonus dice =",Bonus_dice_2 )
+   
+        break       
+
+
+    
+    
